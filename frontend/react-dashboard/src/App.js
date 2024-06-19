e2ce587ee6ba3e0809bcd6cdd5635c9f46805c77
uo271140 <151668616+uo271140@users.noreply.github.com>
2024-06-19 11:48:57 +0000

    Varios prototipos frontend

---
+++ frontend/react-dashboard/src/App.js
@@ -0,0 +1,20 @@
+import { ColorModeContext, useMode } from "./theme";
+import {CssBaseLine, ThemeProvider } from "@mui/material";
+
+function App() {
+
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
